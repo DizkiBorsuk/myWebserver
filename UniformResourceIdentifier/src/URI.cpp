aaf04f603bd3042028307ce57aee055b6c2ba5f9
DizkiBorsuk <maciekrostocki@gmail.com>
2023-08-06 22:36:59 +0200

    finished url parsing

--- UniformResourceIdentifier/src/URI.cpp
+++ UniformResourceIdentifier/src/URI.cpp
@@ -7,12 +7,13 @@ namespace URI
     {   
         std::string scheme; 
         std::string host; 
+        std::vector<std::string> path; 
     };
 
     URI::~URI() = default; // destructor set to default 
 
     URI::URI() : 
-        pImpl_(new Implementation) // pointer to implementation class 
+        pImpl_(new Implementation) // pointer to implementation class allocated at heap 
     {
 
     }
@@ -20,16 +21,43 @@ namespace URI
 
     bool URI::parseString(const std::string &uriString)
     {
+        // parsing url adress to scheme, host and path parts 
         const auto schemeEnd = uriString.find(":"); 
         pImpl_->scheme = uriString.substr(0, schemeEnd); // assign to scheme in implementation class thru pointer
-        if(uriString.substr(schemeEnd,2) =="//")
+        auto restOfURL = uriString.substr(schemeEnd+1); 
+
+
+        if(restOfURL.substr(0,2) =="//")
         {
-            const auto authorityEnd = uriString.find('/', schemeEnd+2); // find / starting from // 
-            pImpl_->host = uriString.substr(schemeEnd+2, authorityEnd - (schemeEnd+2)); 
+            const auto authorityEnd = restOfURL.find('/', 2); // find / starting from // 
+            pImpl_->host = restOfURL.substr(2, authorityEnd -2); 
+            restOfURL = restOfURL.substr(authorityEnd); 
         } 
         else 
         {
+            pImpl_->host.clear(); 
+        }
 
+        pImpl_->path.clear(); 
+
+        if(!restOfURL.empty())
+        {
+            while(true) 
+            {
+                auto pathDelimeter = restOfURL.find('/'); 
+                if(pathDelimeter == std::string::npos) //if finding path delimeter fails
+                {
+                    pImpl_->path.emplace_back(restOfURL); 
+                    break; 
+                }
+                else 
+                {
+                    pImpl_->path.emplace_back(restOfURL.begin(), restOfURL.begin()+pathDelimeter); 
+                }
+                
+                restOfURL = restOfURL.substr(pathDelimeter+1); 
+            }
+            
         }
         
         return true; 
@@ -44,7 +72,7 @@ namespace URI
     }
     std::vector<std::string> URI::getPath() const
     {
-        return {}; 
+        return pImpl_->path; 
     }
 
 }
